27767515dfd96b63b2ba09cc7b5833bcedd5577b
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-21 16:23:13 +0200

    Begun

---
+++ state.py
@@ -0,0 +1,55 @@
+from enum import Enum
+import selflocalize
+from particle import Particle, add_uncertainty, estimate_pose
+
+class RobotState(Enum):
+    lost = 0
+    moving = 1
+    checking = 2
+
+
+
+
+class State:
+
+    def __init__(self) -> None:
+        self.state = RobotState.lost
+        self._lost = self.Lost()
+        self._moving = self.Moving()
+        self._checking = self.Checking()
+        self.current_state = self._lost
+
+    class Lost:
+        def __init__(self) -> None:
+            pass
+
+        def update(self):
+            pass
+
+    class Moving:
+        def __init__(self) -> None:
+            pass
+
+    class Checking:
+        def __init__(self) -> None:
+            pass
+
+    def set_state(self, state: RobotState):
+        self.state = state
+        self.current_state = self.lost or self.moving or self.checking
+
+    @property
+    def lost(self):
+        return self._lost if self.state == RobotState.lost else None
+
+    @property
+    def moving(self):
+        return self._moving if self.state == RobotState.moving else None
+
+    @property
+    def checking(self):
+        return self._checking if self.state == RobotState.checking else None
+
+    def update(self):
+        if self.lost:
+
