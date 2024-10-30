852f209068889013c944cc767bbf1c33b58db91e
norbjerg <120656170+norbjerg@users.noreply.github.com>
2024-10-30 12:48:31 +0100

    Update state.py

--- state.py
+++ state.py
@@ -153,7 +153,7 @@ class State:
                     )
                 )
                 print("dist from target", dist)
-                if dist < 180:
+                if dist < 100:
                     print(
                         "Found target reached. Moving to next target",
                         self.outer_instance.goal_order[self.outer_instance.current_goal + 1],
