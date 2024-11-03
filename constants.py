91cf0148d15b456bf93024c624be02104ed3d67a
ShatinNguyen <shatin2@hotmail.com>
2024-11-03 16:08:00 +0100

    jgjhg

--- constants.py
+++ constants.py
@@ -55,7 +55,7 @@ class Constants:
         QUARTER_TURN_64 = 0.725  # sleep
         FORWARD_SPEED = 100 / 2.7  # cm/s
         ROTATIONAL_SPEED = 0.85  # np.deg2rad(360 / 7.3)  # rad/s
-        DISTANCE_NOISE = 10  # cm
+        DISTANCE_NOISE = 5  # cm
         ANGULAR_NOISE = 0.2  # rad
         CTRL_RANGE = [-20, 20]  # cm
 
