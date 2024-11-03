7902da6b221eb6b86d0fa6b68e16dc9115ee5d1b
ShatinNguyen <shatin2@hotmail.com>
2024-11-03 15:54:37 +0100

    asd

--- command.py
+++ command.py
@@ -136,7 +136,7 @@ class Straight(Command):
         super().__init__(robot, particles)
 
         self.mov_dirs = (1, 1)
-        self.power = 64
+        self.power = 32
 
         if distance < 0:
             self.mov_dirs = (0, 0)
